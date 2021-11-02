2f39f4c4428799e7b488888123bae8a12c0279c3
diyanamendoza <diyana.amp@pm.me>
2021-11-02 11:58:24 -0700

    Update rootdir path

--- __tests__/app-test.js
+++ __tests__/app-test.js
@@ -2,9 +2,8 @@ const request = require('supertest');
 const { rm, mkdir } = require('fs/promises');
 const app = require('../lib/app.js');
 const SimpleDB = require('../lib/simple-db.js');
-const SimpleDb = require('../lib/simple-db.js');
 
-const rootDir = '../store';
+const rootDir = `${__dirname}/../store/shows/`;
 
 describe('shows CRUD API', () => {
   beforeEach(() => {
